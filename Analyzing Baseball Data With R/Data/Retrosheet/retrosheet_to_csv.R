f274dafa605b3384cef9635beb180ec144d16ef2
Connor <connormoffatt4@gmail.com>
2018-12-18 07:22:19 -0500

    Add back Lahman csv's that are ignored

--- Analyzing Baseball Data With R/Data/Retrosheet/retrosheet_to_csv.R
+++ Analyzing Baseball Data With R/Data/Retrosheet/retrosheet_to_csv.R
@@ -2,16 +2,15 @@
 local_path = "C:/Users/conno/Documents/GitHub/Baseball/Analyzing Baseball Data With R/Data/Retrosheet"
 setwd(local_path)
 
-# # Read in parse function written by Authors
-# source("parse.retrosheet.pbp.R")
-# 
-# # Load in 1950 Data
-# parse.retrosheet.pbp(1950)
+# Must create directory download.folder as subdirectory of working folder
+# Then create subdirectories "zipped" and "unzipped" as a subdirectory of
+# download.folder. Appropriate Chadwick files must be downloaded and placed
+# in the unzipped folder:
+# https://sourceforge.net/projects/chadwick/files/chadwick-0.6/chadwick-0.6.5/
 
 # Load in the updated parsing function "parse.retrosheet2.pbp.R"
 library(devtools)
 source_gist(8892981)
-
 
 # Loop and get all data since 2000
 years <- seq(2000, 2018)
